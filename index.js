ad4a8bc836fd35c97addc9d317f54addb1eb2c94
Davy Nyakinyua <davynyakingua@gmail.com>
2025-08-09 16:15:34 +0300

    Create a recursive function to generate fibonacci sequence in an array

--- index.js
+++ index.js
@@ -20,3 +20,29 @@ function fibs (n) {
 }
 
 fibs(8);
+
+// recursive function to create fibonacci sequence
+// input is a number
+// output is an array of fibonacci sequence
+function fibsRec (n) {
+  // base case
+  if(n <= 0){   // base case if n is 0
+    return [];
+  }else if(n === 1){ //base case if n is 1
+    return [0];
+  }else if(n === 2){ // base case is n is 2
+    return [0, 1];
+  }
+
+  // recursive base
+  // reduce n until base case  with n ==2 is active and assign it to sequence
+  const sequence = fibsRec(n -1); 
+  // get next number now that sequence is [0, 1]
+  const nextNum = sequence[sequence.length -1] + sequence[sequence.length -2];
+  // push the next number to sequence array
+  sequence.push(nextNum);
+
+  return sequence;
+};
+
+console.log(fibsRec(8));
